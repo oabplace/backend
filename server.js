d29c5f9b16564c914d4c8a2822ae3d1a84ef4820
oabplace <oabgallery@gmail.com>
2026-05-16 20:59:33 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(cors({
 }));
 
 // Routes
-app.use('/api/bookings', bookingRoutes);
+app.use('/api/bookings/webhook', express.raw({ type: '*/*' }));
+app.use(express.json());
 
 // Test route
 app.get('/', (req, res) => {
@@ -28,3 +29,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
